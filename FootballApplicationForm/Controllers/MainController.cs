86c3ff209c552a7d094ed734aab12c3b9cd6da06
Kamil Chrościcki <k.chroscicki@stud.elka.pw.edu.pl>
2021-03-04 17:34:44 +0100

    Model and Controller added

---
+++ FootballApplicationForm/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballApplicationForm.Controllers
+{
+	public class MainController : Controller
+	{
+		// GET: Main
+		public ViewResult Index()
+		{
+			string time = DateTime.Now.ToString();
+			ViewBag.Time = time;
+			return View();
+		}
+	}
+}
---
+++ FootballApplicationForm/Models/PlayerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballApplicationForm.Models
+{
+	public class PlayerInfo
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Age { get; set; }
+		public string Gender { get; set; }
+		public bool? CurrentlyPlaying { get; set; }
+	}
+}
